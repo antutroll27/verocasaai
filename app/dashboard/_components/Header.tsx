9175fd5e736b7e0a09e551f391359a0581e48431
Antariksha Kumar <30551144+antutroll27@users.noreply.github.com>
2025-01-13 13:48:18 +0000

    header updated with the current usersubscription status

--- app/dashboard/_components/Header.tsx
+++ app/dashboard/_components/Header.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import Image from "next/image";
 import { UserButton } from "@clerk/nextjs";
 import { UserDataContext } from "@/app/_context/UserDataContext";
@@ -7,7 +7,20 @@ import { Users } from "@/config/schema";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
+import { useUser } from "@clerk/nextjs"; // if you're using Clerk
+
+
 function Header() {
+
+
+	const { userDetail, setUserDetail } = useContext(UserDataContext);
+
+	useEffect(() => {
+		if (!userDetail) {
+			console.error("User details are missing or not provided in context");
+		}
+	}, [userDetail]);
+
 	return (
 		<div className="p-10 flex justify-around items-center ">
 			{/* Logo Section */}
@@ -40,14 +53,20 @@ function Header() {
 					Buy Credits
 				</Link>
 
-				<div className="flex gap-3 items-center bg-colors-custom-purple px-4 py-1 rounded-md">
-					<div className="bg-colors-custom-pink rounded-full px-1 py-1">
-						<Image src="/star.svg" alt="credits" width={12} height={12} />
-					</div>
+				<div className="flex gap-3 items-center bg-colors-custom-pink px-4 py-1 rounded-md">
+					
 
-					{/*<h2 className="text-white font-bold text-xl">{typeof userData?.credits === 'number' ? userData.credits : 0}</h2>*/}
-					<h2 className="text-white font-bold text-l">FREE till 31st DEC</h2>
-					{/*<h2 className="text-colors-custom-pastel text-xs pt-1">Credits</h2>*/}
+					
+					<h2 className={`font-bold text-l ${userDetail?.subscriptionType === "free"
+							? "text-red-500"
+							: "text-green-500"
+						}`}>
+						{userDetail?.subscriptionType === "free"
+							? "FREE TIER"
+							: "PREMIUM TIER"
+						}
+					</h2>
+					
 				</div>
 
 				{/* User Button */}
