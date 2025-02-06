582bd94202c0d7af1021eda1f32b3a705eee6486
Antariksha Kumar <30551144+antutroll27@users.noreply.github.com>
2025-02-06 18:13:43 +0530

    minor bug fixes

--- components/ui/CookieBot.tsx
+++ components/ui/CookieBot.tsx
@@ -1,23 +1,12 @@
 'use client';
 import ReactCookieBot from "react-cookiebot";
-import { useEffect } from 'react';
 
 const ClientCookieBot = () => {
   const COOKIE_BOT_ID = "1676e844-56d7-414d-83b3-e79ae790129b";
-  
-  useEffect(() => {
-    console.log('CookieBot mounting with ID:', COOKIE_BOT_ID);
-    // Log if CookieBot script is loaded
-    const cookiebotScript = document.querySelector('script[src*="cookiebot.com"]');
-    console.log('CookieBot script found:', !!cookiebotScript);
-  }, []);
-
   return (
     <ReactCookieBot 
       domainGroupId={COOKIE_BOT_ID}
       language="en"
-      culture="en"
-      debug={true}
     />
   );
 };
