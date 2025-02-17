b2f82f3e5a3b29a65aa9b83ec1c41489ccc3878c
Antariksha Kumar <30551144+antutroll27@users.noreply.github.com>
2025-02-17 16:04:06 +0000

    Updated the dashboard UI
    Improved the UI for overall User Experience purposes ( check UI )

--- app/dashboard/_components/RoomRedesigns.tsx
+++ app/dashboard/_components/RoomRedesigns.tsx
@@ -8,6 +8,8 @@ interface Room {
   OgImage: string;
   roomType: string;
   AIRedesignType: string;
+  id: number;
+  createdAt: string;
 }
 
 function RoomRedesigns({ room }: { room: Room }) {
@@ -26,9 +28,17 @@ function RoomRedesigns({ room }: { room: Room }) {
         secondImage={{
           imageUrl: room?.OgImage
         }} />
-      <div>
-        <h2>Room Type {room?.roomType}</h2>
-        <h2>Design Type{room?.AIRedesignType}</h2>
+      <div className="bg-pink-200 p-4 mt-2">
+        <div className="text-sm font-light mb-2">#{room?.id}</div>
+        <div className="text-right text-sm font-light">{room?.createdAt}</div>
+        <div>
+          <span className="text-base font-bold">Room Type: </span>
+          <span className="text-base font-bold">{room?.roomType}</span>
+        </div>
+        <div>
+          <span className="text-base font-bold">Design Type: </span>
+          <span className="text-base font-bold">{room?.AIRedesignType}</span>
+        </div>
       </div>
       <OutputImageDialog
         openImageDialog={openDialog}
