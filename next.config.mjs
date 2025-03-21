51ac635cac4e1c0fa78bf115fad112a5aea2c12f
Antariksha Kumar <30551144+antutroll27@users.noreply.github.com>
2025-03-21 16:15:33 +0000

    update next.config.mjs
    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Add transpilePackages for Sanity components
+  transpilePackages: ['next-sanity'],
+  // Add Sanity content sources to be included in build
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        port: '',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
